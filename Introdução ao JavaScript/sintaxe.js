df80edd82266a5629d3e75ece8391bb9ae287116
Renan Santos <renan.donizeti@hotmail.com>
2022-06-01 11:38:01 -0300

    Introdução em javascript

---
+++ Introdução ao JavaScript/sintaxe.js
@@ -0,0 +1,29 @@
+// comentário uma linha - ctrl + barra
+
+/* Comentário de vários intens */
+
+//var o valor pode ser manipulado
+
+var preco = 2;
+
+var desconto = 0.2;
+
+var preco = preco - desconto
+
+//const valor não pode ser manipulado
+
+const PRECO = 2;
+
+var desconto = 0.2;
+
+var total = PRECO - desconto
+
+
+//sintaxe função
+
+function soma(a,b) {
+    return a + b; // 8
+}
+
+soma(3, 5);
+console.log(soma)
